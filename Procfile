3a355d98a9d69ad6abe77ee1ad5c53011cc1ef0e
David Albert <davidbalbert@gmail.com>
2013-09-25 17:48:57 -0400

    App skeleton

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec unicorn --no-default-middleware --port $PORT --config-file ./config/unicorn.rb
---
+++ config/unicorn.rb
@@ -0,0 +1,42 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 1)
+
+if ENV["RAILS_ENV"] == "development"
+  timeout 30 * 60 * 60 * 24 # 30 days, max timeout
+else
+  timeout 30
+end
+
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+  #
+  # Note: The Dalli gem (memcached client) automatically detects
+  # shared sockets based on the current PID. We use it in
+  # production, but it's not necessary to reconnect here. -Dave
+end
